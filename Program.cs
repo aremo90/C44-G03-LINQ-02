d0c64629d94d0d2997c2204c52114d0cd8036cfb
aremo90 <mrhamza2005@gmail.com>
2025-09-01 20:22:02 +0300

    till 09 Let & into

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using ConsoleApp1.Data;
 using System.Collections;
+using System.Text.RegularExpressions;
 
 namespace ConsoleApp1
 {
@@ -83,6 +84,173 @@ namespace ConsoleApp1
             //foreach (var item in Result)
             //    Console.WriteLine(item);
             #endregion
+
+            #region Quantifier Operators
+
+            // Deferred Execution
+
+            //var Result = ListGenerator.ProductList.All(p => p.UnitsInStock > 0); // AND
+            //var Result = ListGenerator.ProductList.Any(p => p.UnitsInStock > 0); // OR
+            //var Result = ListGenerator.ProductList.SequenceEqual(ListGenerator.ProductList2); // Compare two sequences
+            //var sequence1 = Enumerable.Range(10, 100);// % 10 - 109
+            //var sequence2 = Enumerable.Range(60, 100);// % 60 - 159
+            //var Result = sequence1.SequenceEqual(sequence2); // Compare two sequences
+
+            //Console.WriteLine(Result);
+
+            #endregion
+
+            #region Zipping Operators
+
+            // Deferred Execution
+
+            //string[] Names = { "Ahmed", "Ali", "Sayed" , "Omar"};
+            //int[]    Ages  = Enumerable.Range(1, 10).ToArray();
+            //char[] Chars = { 'A', 'B', 'C', 'D'};
+
+            //var Result = Names.Zip(Chars);
+
+            //var Result = Names.Zip(Ages, Chars);
+
+
+            //foreach (var item in Result)
+            //    Console.WriteLine(item);
+
+            #endregion
+
+            #region Grouping Operators
+
+            // Deferred Execution
+            // GroupBy
+
+            #region
+
+            //var Result = ListGenerator.ProductList.GroupBy(p => p.Category);
+
+            //foreach(var group in Result)
+            //    {
+            //        Console.WriteLine($"Category : {group.Key} - Count : {group.Count()}");
+            //        foreach (var product in group)
+            //            Console.WriteLine($"\t{product.ProductName}");
+            //}
+
+
+            #endregion
+
+            #region Get Products That is Out of stock grouped by Category
+
+            //var Result = ListGenerator.ProductList
+            //                          .Where(p => p.UnitsInStock == 0)
+            //                          .GroupBy(p => p.Category);
+
+
+
+            #endregion
+
+
+            #region get Product in stock grouped by Category that contain more than 10 products
+
+            //var Result = ListGenerator.ProductList
+            //                          .Where(p => p.UnitsInStock > 0)
+            //                          .GroupBy(p => p.Category)
+            //                          .Where(g => g.Count() > 10);
+
+            #endregion
+
+            #region 
+
+            //var Result = ListGenerator.ProductList
+            //                          .Where(p => p.UnitsInStock > 0)
+            //                          .GroupBy(p => p.Category)
+            //                          .Where(g => g.Count() > 10)
+            //                          .Select(X => new 
+            //                          {
+            //                            CategoreyName = X.Key,
+            //                            CountOfProuct = X.Count(),
+            //                          });
+
+            //foreach (var group in Result)
+            //{
+            //    Console.WriteLine($"Category : {group.CategoreyName} - Count : {group.CountOfProuct}");
+            //}
+
+            #endregion
+
+
+            //foreach (var group in Result)
+            //{
+            //        Console.WriteLine($"Category : {group.Key} - Count : {group.Count()}");
+            //        foreach (var product in group)
+            //            Console.WriteLine($"\t{product.ProductName}");
+            //}
+
+            #endregion
+
+            #region Partitioning Operators
+
+            //var Result = ListGenerator.ProductList.Where(p => p.UnitsInStock == 0).Take(20);
+            //var Result = ListGenerator.ProductList.Skip(20);
+            //var Result = ListGenerator.ProductList.Skip(10).Take(10);
+
+            //var Result = ListGenerator.ProductList.TakeLast(10);
+            //var Result = ListGenerator.ProductList.SkipLast(10);
+
+            //var page01 = ListGenerator.ProductList.Skip(0).Take(10);
+            //var page02 = ListGenerator.ProductList.Skip(10).Take(10);
+            //var page03 = ListGenerator.ProductList.Skip(20).Take(10);
+
+            //Console.WriteLine("======================== Page01 =================");
+            //foreach (var product in page01)
+            //    Console.WriteLine(product.ProductName);
+
+            //Console.WriteLine("======================== Page02 =================");
+            //foreach (var product in page02)
+            //    Console.WriteLine(product.ProductName);
+
+            //Console.WriteLine("======================== Page03 =================");
+            //foreach (var product in page02)
+            //    Console.WriteLine(product.ProductName);
+
+            //int[] Numbers = { 5,3,4,6,1,3,8,9 };
+
+            //var Result = Numbers.TakeWhile((n, index) => n > index);
+            //var Result = Numbers.SkipWhile((n, index) => n > index);
+            //foreach (var number in Result)
+            //    Console.WriteLine(number);
+
+
+
+            #endregion
+
+            #region Let & Into Keywords
+
+            //List<string> Names = new List<string>
+            //{
+            //    "Ahmed",
+            //    "Ali",
+            //    "Sayed",
+            //    "Omar",
+            //    "Mahmoud",
+            //    "Ayman",
+            //    "Hossam"
+            //};
+            // remove vowel Chars
+
+            //var Result = from n in Names
+            //             select Regex.Replace(n, "[aeiouAEIOU]", string.Empty )
+            //             into nameWithoutVowel
+            //                where nameWithoutVowel.Length > 3
+            //                select nameWithoutVowel;
+            //var Result = from n in Names
+            //             let nameWithoutVowel = Regex.Replace(n, "[aeiouAEIOU]", string.Empty )
+            //             where nameWithoutVowel.Length > 3
+            //             select nameWithoutVowel;
+
+            //foreach (var name in Result)
+            //    Console.WriteLine(name);
+
+            #endregion
+
         }
     }
 }
